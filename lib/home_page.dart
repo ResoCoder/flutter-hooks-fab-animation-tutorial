a81f66feddc125962ebd01121ddcddd5c1613c26
Matt <mattresetar@resocoder.com>
2020-01-20 08:37:26 +0100

    Starter Project

---
+++ lib/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  ScrollController _scrollController;
+  AnimationController _hideFabAnimController;
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _hideFabAnimController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _hideFabAnimController = AnimationController(
+      vsync: this,
+      duration: kThemeAnimationDuration,
+      value: 1, // initially visible
+    );
+
+    _scrollController.addListener(() {
+      switch (_scrollController.position.userScrollDirection) {
+        // Scrolling up - forward the animation (value goes to 1)
+        case ScrollDirection.forward:
+          _hideFabAnimController.forward();
+          break;
+        // Scrolling down - reverse the animation (value goes to 0)
+        case ScrollDirection.reverse:
+          _hideFabAnimController.reverse();
+          break;
+        // Idle - keep FAB visibility unchanged
+        case ScrollDirection.idle:
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Let's Scroll"),
+      ),
+      floatingActionButton: FadeTransition(
+        opacity: _hideFabAnimController,
+        child: ScaleTransition(
+          scale: _hideFabAnimController,
+          child: FloatingActionButton.extended(
+            label: const Text('Useless Floating Action Button'),
+            onPressed: () {},
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: ListView(
+        controller: _scrollController,
+        children: <Widget>[
+          for (int i = 0; i < 5; i++)
+            Card(child: FittedBox(child: FlutterLogo())),
+        ],
+      ),
+    );
+  }
+}
